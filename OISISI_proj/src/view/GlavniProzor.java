71f09d22fdb257ff303809267e573463eb07c9cb
Nikola Milosavljević <milosavljevic.ra5.2018@uns.ac.rs>
2020-11-14 19:30:26 +0100

    Dodat glavni prozor i toolbar

---
+++ OISISI_proj/src/view/GlavniProzor.java
@@ -0,0 +1,24 @@
+package view;
+
+//importi :
+import javax.swing.*;
+import java.awt.*;
+
+public class GlavniProzor extends JFrame {
+	
+	public GlavniProzor() {
+		Toolkit kit = Toolkit.getDefaultToolkit();
+		Dimension screenSize = kit.getScreenSize();
+		int screenHeight = screenSize.height * 3/4;
+		int screenWidth = screenSize.width * 3/4;
+		
+		setSize(screenWidth,screenHeight);
+		
+		setTitle("Studentska služba");
+		
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		setLocationRelativeTo(null);
+	}
+	
+}
