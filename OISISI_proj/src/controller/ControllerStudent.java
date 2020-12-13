05d9a75cf98f3a9315d356afca60d39a8d6bed1a
AnicaDjukic <djukic.ra20.2018@uns.ac.rs>
2020-12-13 19:31:36 +0100

    #dodavanje_studenta Izmene vezane za dodavanje studenta i prikaz studenata

--- OISISI_proj/src/controller/ControllerStudent.java
+++ OISISI_proj/src/controller/ControllerStudent.java
@@ -1,8 +1,6 @@
 package controller;
 
-import java.awt.*;
 import java.util.*;
-import javax.swing.*;
 
 import model.Ocena;
 import model.Predmet;
@@ -10,55 +8,43 @@ import model.Student;
 
 public class ControllerStudent {
 	
-	//Sluzi za upravljanjem globalne liste studenata
+	//Sluzi za upravljanje listom studenata
 	
 	ArrayList<Student> listaStudenti;
-	
+		
 	public ControllerStudent() {
 		listaStudenti = new ArrayList<Student>();
-		
+			
 		//Initialize();
 	}
-	
-	public ArrayList<Student> getListaStudenta() {
+		
+	public ArrayList<Student> getListaStudenata() {
 		return this.listaStudenti;
 	}
-	
+		
 	public void Initialize() {
 		/*for(int i = 0; i < 10; i++) {
 			Student p = new Student();
 			p.setIme("" + i);
 			listaStudenti.add(p);
-		}*/
+         }*/
 	}
-	
+		
 	public boolean dodajStudenta(Student s) {
 		if(listaStudenti.isEmpty()) {
 			listaStudenti.add(s);
 			return true;
 		}
-		
+			
 		for(Student student: listaStudenti) {
 			if(student.getBrIndexa().equals(s.getBrIndexa())) {
 				return false;
 			}
-				
+					
 		}
 
 		listaStudenti.add(s);
 		return true;
-	}
-	
-	public JPanel createPanel(JLabel label, JTextField text) {
-		JPanel panel = new JPanel();
-		
-		label.setPreferredSize(new Dimension(150, 25));
-		panel.add(label);
-		
-		text.setPreferredSize(new Dimension(200, 25));
-		panel.add(text);
-		
-		return panel;
 	}
 	
 	public void obrisiPredmetKodSvihStud(String spr) {
@@ -76,15 +62,4 @@ public class ControllerStudent {
 		}
 	}
 	
-	public JPanel createListPanel(JLabel label, JComboBox<String> text) {
-		JPanel panel = new JPanel();
-		
-		label.setPreferredSize(new Dimension(150, 25));
-		panel.add(label);
-		
-		text.setPreferredSize(new Dimension(200, 25));
-		panel.add(text);
-		
-		return panel;
-	}
 }
--- OISISI_proj/src/view/TabelaStudenti.java
+++ OISISI_proj/src/view/TabelaStudenti.java
@@ -1,39 +1,27 @@
 package view;
 
-//import controller.ControllerStudent;
 import java.util.ArrayList;
 
 import javax.swing.JTable;
 import javax.swing.table.DefaultTableModel;
 
 import controller.ControllerStudent;
+import model.Profesor;
 import model.Student;
 
 public class TabelaStudenti extends JTable {
 	
+	static ControllerStudent controller;
 	
-	private static DefaultTableModel tabela;
+	private static String[] cols = {"Indeks", "Ime", "Prezime", "Godina studija", "Status", "Prosek"};
+	static DefaultTableModel model;
+	static TabelaStudenti table;
 	
-	public TabelaStudenti () {
-		Object[] cols = {"Indeks", "Ime", "Prezime", "Godina studija", "Status", "Prosek"};
-		tabela = new DefaultTableModel() {
-			
-			public boolean isCellEditable(int row, int col) {
-				return false;
-			}
-		};
-		tabela.setColumnIdentifiers(cols);
-		setModel(tabela);
+	public TabelaStudenti() {
+		table = this;
+		controller = GlavniProzor.getControllerStudent();
 		
-		setRowHeight(35);
-		setAutoCreateRowSorter(true);
-		setFont(getFont().deriveFont(16f));
-		
-		// Centriranje
-		NasCellRenderer prikaz = new NasCellRenderer(NasCellRenderer.studentRenderer);
-		for(Object o : cols) {
-			getColumn(o).setCellRenderer(prikaz);
-		}
+		initializeTable(table);		
 		
 		// Primer
 		/*Object[] prvi = {"RA15/2018", "Pera", "Peric", "3", "B" , "9.5"};
@@ -42,17 +30,42 @@ public class TabelaStudenti extends JTable {
 		azurirajTabelu();*/
 	}
 	
-	public static boolean dodajStudentaUTabelu(Student s, ControllerStudent cs) {
-		String[] data = { s.getBrIndexa(), s.getIme(), s.getPrezime(), "", "", ""};
-		data[3] = Integer.toString(s.getTrenutnaGodStud());;
-		data[4] = s.getStatus();
-		data[5] = Double.toString(s.getPosecnaOcena());
-
-		if(!cs.dodajStudenta(s))
-			return false;
+	public static void initializeTable(TabelaStudenti table) {
 		
-		tabela.addRow(data);
-		return true;
+		model = new DefaultTableModel() {
+			
+			public boolean isCellEditable(int row, int col) {
+				return false;
+			}
+		};
+		
+		model.setColumnIdentifiers(cols);
+		table.setModel(model);
+		
+		table.setRowHeight(35);
+		table.setAutoCreateRowSorter(true);
+		table.setFont(table.getFont().deriveFont(16f));
+		
+		// Centriranje
+		NasCellRenderer prikaz = new NasCellRenderer(NasCellRenderer.studentRenderer);
+		for(Object o : cols) {
+			table.getColumn(o).setCellRenderer(prikaz);
+		}
+	}
+	
+	public static void updateTable() {
+		ArrayList<Student> listaStudenata = controller.getListaStudenata();
+
+		initializeTable(table);
+		
+		for(Student s : listaStudenata) {
+			String[] data = { s.getBrIndexa(), s.getIme(), s.getPrezime(), "", "", ""};
+			data[3] = Integer.toString(s.getTrenutnaGodStud());;
+			data[4] = s.getStatus();
+			data[5] = Double.toString(s.getPosecnaOcena());
+			
+		    model.addRow(data);
+		}
 	}
 	
 }
