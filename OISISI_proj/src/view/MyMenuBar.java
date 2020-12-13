17d0ec7a68fca54289aed871c3bca419787f8d15
AnicaDjukic <djukic.ra20.2018@uns.ac.rs>
2020-12-13 18:57:01 +0100

    #menu_bar Brisanje nepotrebnih importa

--- OISISI_proj/src/view/MyMenuBar.java
+++ OISISI_proj/src/view/MyMenuBar.java
@@ -4,18 +4,15 @@ import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
 import javax.swing.JOptionPane;
 import javax.swing.KeyStroke;
-import javax.swing.SwingConstants;
 
 import controller.*;
 import model.GlobalConstants;
 
-import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 
 import javax.swing.ImageIcon;
-import javax.swing.JLabel;
 import javax.swing.JMenu;
 
 public class MyMenuBar extends JMenuBar {
