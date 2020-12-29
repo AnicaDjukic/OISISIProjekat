53d6cda4fe21cf86512a3e94c558fa66479bc90f
AnicaDjukic <djukic.ra20.2018@uns.ac.rs>
2020-12-29 01:34:42 +0100

    #izmena_studenta Izmene vezane za validaciju

--- OISISI_proj/src/controller/StudentFocusListeners.java
+++ OISISI_proj/src/controller/StudentFocusListeners.java
@@ -72,31 +72,38 @@ public class StudentFocusListeners implements FocusListener {
 	
 	public void enableOrDisableButton() {
 		boolean enableButton = true;
-		if(AddOrEditStudent.tIme.getForeground() == Color.RED || AddOrEditStudent.tIme.getText().trim().equals(""))
+		if(!Checker.isNameOrSurename(AddOrEditStudent.tIme.getText().trim())  || AddOrEditStudent.tIme.getText().trim().equals("")) {
+			AddOrEditStudent.tIme.setForeground(Color.RED);
 			enableButton = false;
-		
-		if(AddOrEditStudent.tPrezime.getForeground() == Color.RED || AddOrEditStudent.tPrezime.getText().trim().equals(""))
+		}
+		if(!Checker.isNameOrSurename(AddOrEditStudent.tPrezime.getText().trim()) || AddOrEditStudent.tPrezime.getText().trim().equals("")) {
+			AddOrEditStudent.tPrezime.setForeground(Color.RED);
 			enableButton = false;
-		
-		if(AddOrEditStudent.tDatRodj.getForeground() == Color.RED || AddOrEditStudent.tDatRodj.getText().trim().equals(""))
+		}
+		if(!Checker.isValidDate(AddOrEditStudent.tDatRodj.getText().trim()) || AddOrEditStudent.tDatRodj.getText().trim().equals("")) {
+			AddOrEditStudent.tDatRodj.setForeground(Color.RED);
 			enableButton = false;
-		
-		if(AddOrEditStudent.tAdrStan.getForeground() == Color.RED || AddOrEditStudent.tAdrStan.getText().trim().equals(""))
+		}
+		if(!Checker.isValidAdrress(AddOrEditStudent.tAdrStan.getText().trim()) || AddOrEditStudent.tAdrStan.getText().trim().equals("")) {
+			AddOrEditStudent.tAdrStan.setForeground(Color.RED);
 			enableButton = false;
-		
-		if(AddOrEditStudent.tBrTel.getForeground() == Color.RED || AddOrEditStudent.tBrTel.getText().trim().equals(""))
+		}
+		if(!Checker.isValidNumber(AddOrEditStudent.tBrTel.getText().trim(),0) || AddOrEditStudent.tBrTel.getText().trim().equals("")) {
+			AddOrEditStudent.tBrTel.setForeground(Color.RED);
 			enableButton = false;
-			
-		if(AddOrEditStudent.tEmail.getForeground() == Color.RED || AddOrEditStudent.tEmail.getText().trim().equals(""))
+		}
+		if(!Checker.isValidEmail(AddOrEditStudent.tEmail.getText().trim()) || AddOrEditStudent.tEmail.getText().trim().equals("")) {
+			AddOrEditStudent.tEmail.setForeground(Color.RED);
 			enableButton = false;
-		
-		if(AddOrEditStudent.tBrIndexa.getForeground() == Color.RED || AddOrEditStudent.tBrIndexa.getText().trim().equals(""))
+		}
+		if(!regularInput(GlobalConstants.indexLab, AddOrEditStudent.tBrIndexa.getText().trim()) || AddOrEditStudent.tBrIndexa.getText().trim().equals("")) {
+			AddOrEditStudent.tBrIndexa.setForeground(Color.RED);
 			enableButton = false;
-		
-		if(AddOrEditStudent.tGodUpisa.getForeground() == Color.RED || AddOrEditStudent.tGodUpisa.getText().trim().equals(""))
+		}
+		if(!regularInput(GlobalConstants.upisLab, AddOrEditStudent.tGodUpisa.getText().trim()) || AddOrEditStudent.tGodUpisa.getText().trim().equals("")) {
+			AddOrEditStudent.tGodUpisa.setForeground(Color.RED);
 			enableButton = false;
-		
-		
+		}
 		AddOrEditStudent.potvrdi.setEnabled(enableButton);
 	}
 	
